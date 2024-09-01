97a8e6f00852816b512cc932ce7fbaeecdc247dd
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-09-01 14:05:49 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -83,6 +83,25 @@ color.forEach((item)=>{
     item.classList.toggle('active');
   })
 })
+
+// input number
+
+let increment = Array.from(document.getElementsByClassName('increment'));
+let decrement = Array.from(document.getElementsByClassName('decrement'));
+
+increment.forEach((item)=>{
+  item.addEventListener('click', function () {
+    item.previousElementSibling.value ++;
+  })
+});
+decrement.forEach((item)=>{
+  item.addEventListener('click', function () {
+    if (item.nextElementSibling.value > 1) {
+      item.nextElementSibling.value --;
+    }
+  })
+});
+
 // see all
 
 let seeAll= Array.from(document.getElementsByClassName('seeAll'));
